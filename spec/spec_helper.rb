d54d7410432e4a626caede782ad6f3194d27e39f
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-10-01 20:23:13 +0200

    Add some stuff for specs to run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'mocha'
 require 'puppet'
 require 'rspec'
 require 'spec/autorun'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
 
 Spec::Runner.configure do |config|
     config.mock_with :mocha
