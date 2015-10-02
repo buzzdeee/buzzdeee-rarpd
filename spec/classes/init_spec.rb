815611133a2e764e67458cb684b41109fda598c3
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-10-02 12:30:05 +0200

    init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
+
 describe 'rarpd' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('rarpd') }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { is_expected.to compile.with_all_deps }
+    end
   end
 end
